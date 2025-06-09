b38c960816264b69ffafc983a3c1d423956ff240
ale.montaldo <ale.montaldo@gmail.com>
2025-06-09 09:26:14 +0200

    little cleanup

--- src/test/java/com/alesmontaldo/network_controller/domain/device/persistance/mongo_db/DeviceMongoRepositoryTest.java
+++ src/test/java/com/alesmontaldo/network_controller/domain/device/persistance/mongo_db/DeviceMongoRepositoryTest.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class DeviceMongoRepositoryTest {
 
     private static final Log log = LogFactory.getLog(DeviceMongoRepositoryTest.class);
